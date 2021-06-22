56218f1ab46b08d7d22e4e5d9430fd091940248d
Николай Маршанский <ureinhaven@yandex.ru>
2021-06-22 22:09:26 +0300

    Опечатка в названии основного проекта

--- foodgam/recipe/context_processors.py
+++
@@ -1,8 +0,0 @@
-def shop_list_count(request):
-    try:
-        count = request.user.users_shop.count()
-    except AttributeError:
-        count = 0
-    return {
-        'shop_list_count': int(count)
-    }
